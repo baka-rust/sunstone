40d9dc9e034f0f9877e0478b0b3252f4c476addc
Sam Frank <rust@0xio.org>
2014-04-07 16:49:41 -0500

    added NetworkedMonster to client

---
+++ client/include/NetworkedMonster.h
@@ -0,0 +1,34 @@
+#ifndef NETWORKEDMONSTER_H
+#define NETWORKEDMONSTER_H
+
+#include <string>
+#include <vector>
+#include <map>
+
+#include "AnimationSequence.h"
+#include "Terrain.h"
+
+class NetworkedMonster {
+
+	public:
+		
+		float x, y;
+		int tileX, tileY;
+		int speed = 40;
+		std::string direction;
+		bool onTile = true;
+		
+		sf::Sprite spriteLeft, spriteRight, spriteUp, spriteDown;
+		
+		NetworkedMonster(int xPos, int yPos, std::string dir, int type);
+		void update(float elapsedTime);
+		void draw(sf::RenderWindow *app);
+		
+	private:
+
+        std::map<std::string, AnimationSequence*> animations;
+        typedef std::map<std::string, AnimationSequence*>::iterator i_animations;
+
+};
+
+#endif // NETWORKEDMONSTER_H
