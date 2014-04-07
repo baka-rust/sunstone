e75986b57423f0655ff37e650f7d4e131c703322
Morgan Redding <evangambit@gmail.com>
2014-04-07 17:31:06 -0500

    modifed server dungeon and room. Started pathfinder3000

--- server/include/Dungeon.h
+++ server/include/Dungeon.h
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 
 #include "Room.h"
+#include "Tilemap/TileTypes.h"
 
 /*
  0 = empty
@@ -15,32 +16,63 @@
  2 = door
  */
 
-
 class Dungeon {
-
-    public:
-
-        Dungeon(int width, int height, int minRoomSize, int maxRoomSize, int seed);
-        void create();
-        int getMid(int x, int y);
-
-    private:
-
-        std::vector< std::vector<int> > grid;
-        std::vector<Room> rooms;
-        std::vector<Door> doors;
-        int roomsLength;
-        void carveSquare(int x, int y, int width, int height, int value);
-        bool checkSquare(int x, int y, int width, int height, int value);
-        bool addBranch(Room r);
-        void printLine(int y);
-        void printGrid();
-        int randInt(int min, int max);
-        int minRoomSize;
-        int maxRoomSize;
-        int lastRandomNumber;
-        int getRandomNumber();
-
+    
+public:
+    
+    Dungeon(int width, int height, int minRoomSize, int maxRoomSize, int seed);
+    void create();
+    
+    // what room is (x,y) in?  returns -1 if not in any rooms
+    int getRoom(int x, int y);
+    
+    // get bitmap of whether tile[i]==value
+    std::vector< std::vector<bool> > getBitmap(int value);
+    
+    // get a bitmap of features
+    std::vector< std::vector<bool> > getWalls();
+    std::vector< std::vector<bool> > getFloors();
+    
+    // returns a 2D int-grid of distances from (x,y) in the room (x,y) is in
+    std::vector< std::vector<int> > getPathMap(int x, int y);
+    
+    // accessors
+    int width() { return (int) grid.size(); };
+    int height() { return (int) grid[0].size(); };
+    TileType getTile(int x, int y);
+    bool isFloor(int x, int y);
+    bool isWall(int x, int y);
+    
+    
+private:
+    
+    std::vector< std::vector<TileType> > grid;
+    std::vector<Room> rooms;
+    std::vector<Door> doors;
+    
+    int roomCount;
+    int minRoomSize;
+    int maxRoomSize;
+    
+    // used as seed for next random number
+    int lastRandomNumber;
+    
+    
+    // dungeon creation methods
+    int createAttempt();
+    bool addBranch(Room r);
+    void carveSquare(int x, int y, int width, int height, TileType value);
+    bool checkSquare(int x, int y, int width, int height, TileType value);
+    void decorateRoom(Room room);
+    
+    // random numbers
+    int getRandomNumber();
+    int randInt(int min, int max);
+    
+    // printing (for debugging)
+    void printLine(int y);
+    void printGrid();
+    
 };
 
 #endif // DUNGEON_H
--- server/include/Room.h
+++ server/include/Room.h
@@ -7,31 +7,32 @@ struct Door {
 };
 
 class Room {
-
-    public:
-
-        Room(int x, int y, int width, int height);
-        int x() { return _x; };
-        int y() { return _y; };
-        int height() { return _height; };
-        int width() { return _width; };
-        int endX() { return _endX; };
-        int endY() { return _endY; };
-        void addConnectedRoom(Room room, Door door);
-        bool inRoom(int x, int y);
-
-    private:
-
-        int _x;
-        int _y;
-        int _width;
-        int _height;
-        int _endX;
-        int _endY;
-        int _numberOfConnectedRooms = 0;
-        Room* _connectedRooms[4];
-        Door _doors[4];
-
+    
+public:
+    
+    Room(int x, int y, int width, int height);
+    void addConnectedRoom(Room room, Door door);
+    bool inRoom(int x, int y);
+    
+    int x() { return _x; };
+    int y() { return _y; };
+    int width() { return _width; };
+    int height() { return _height; };
+    int endX() { return _endX; };
+    int endY() { return _endY; };
+    
+private:
+    
+    int _x;
+    int _y;
+    int _width;
+    int _height;
+    int _endX;
+    int _endY;
+    int _numberOfConnectedRooms = 0;
+    Room* _connectedRooms[4];
+    Door _doors[4];
+    
 };
 
 #endif // ROOM_H
