f1fa8b2d282f61a0633926009c66d12698fb9054
Ben <ben@stolovitz.com>
2014-04-17 16:06:39 -0500

    Spacing

--- client/include/Player.h
+++ client/include/Player.h
@@ -22,29 +22,24 @@ extern bool focused;
 class Player {
 
     public:
-
         float x, y;
         int tileX, tileY;
         int speed = 40; // rely on stats later
+        bool onTile = true;
+    
         Direction direction;
         State state;
-        bool onTile = true;
 
         Terrain *terrain; // we should figure out a better way to handle this. singletons in cpp are dumb.
         Network *network;
 
-        sf::Sprite spriteLeft, spriteRight, spriteUp, spriteDown;
-
         Player(int xPos, int yPos, std::string dir);
+    
         void update(float elapsedTime);
         void draw(sf::RenderWindow *app);
 
     private:
-
         std::vector<std::vector<AnimationSequence*> > animations = std::vector<std::vector<AnimationSequence*>>(4);
-        typedef std::vector<std::vector<AnimationSequence*> >::iterator i_anim_row;
-        typedef std::vector<AnimationSequence*>::iterator i_anim_col;
-
 };
 
 #endif // PLAYER_H
